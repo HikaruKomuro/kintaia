d2b3409db362581ceaa6f24f9a539ff717863566
EC2 Default User <ec2-user@ip-172-31-2-143.us-east-2.compute.internal>
2019-11-18 02:09:36 +0000

    finish

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -3,8 +3,10 @@ module AttendancesHelper
   def worked_time(start1, finish1, change_date)
     if change_date == 1
       format("%.2f", (finish1 - start1)/3600)
-    else
+    elsif (((finish1.hour*60) + finish1.min - (start1.hour*60) - start1.min)/60) >= 0
       format("%.2f", (finish1.hour*60 + finish1.min - start1.hour*60 - start1.min)/60.00)
+    else
+      format("%.2f", ((finish1.hour*60) + finish1.min + (24*60) - (start1.hour*60) - start1.min)/60.00)
     end
   end
   
